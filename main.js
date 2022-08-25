dc30e3f523c1fa156d8574a9817452882d436e3a
ShivaCare <111869417+ShivaCare@users.noreply.github.com>
2022-08-25 08:34:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,22 @@
-Function back(){
+function bak(){
 //create the images variable to run time values
 let images = 3;
 //create the directory to pickup the images
-lat dir = "img/";
+let dir = "img/";
 //create the random choice number function used Math.random method
-lat randomChoice = (Math.floor(Math.random()*images));
+let randomChoice =(Math.floor(Math.random()*images));
 //create tha arrays to create the images callections for mobiles
 let mobile = ['Pic0.jpg','Pic1.jpg','Pic6.jpg'];
 //create tha arrays to create the images callections for desktop
-let desktop =  ['Pic3.jpg','Pic5.jpg','Pic7.jpg'];
+let desktop = ['Pic3.jpg','Pic5.jpg','Pic7.jpg'];
 //define the media qurey 
-let win = window.matchMedia("(max-width: 700px"));
+let win = window.matchMedia("(max-width: 700px)");
 //check the media qurey is true & glase
 if(win.matches){
 //I change background images with setAttribute with media qurey
-document.getElementById("header").setAttribute("style","background-image:linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.7)), url(" + dir + mobile[randomChoice]");");
+document.getElementById("header").setAttribute("style","background-image:linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.7)), url(" + dir + mobile[randomChoice]+");");
 }else{
-document.getElementById("header").setAttribute("style","background-image:linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.7)), url(" + dir + desktop[randomChoice]");");
+document.getElementById("header").setAttribute("style","background-image:linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.7)), url(" + dir + desktop[randomChoice]+")");
+} 
 }
-
-
-
-
-
-}
-back();
+bak();
