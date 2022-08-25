18051836a3deade19f47a612b3f11a174287a2d3
ShivaCare <111869417+ShivaCare@users.noreply.github.com>
2022-08-25 08:13:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,11 +5,19 @@ let images = 3;
 lat dir = "img/";
 //create the random choice number function used Math.random method
 lat randomChoice = (Math.floor(Math.random()*images));
-//create tha arrays to create the images callections
+//create tha arrays to create the images callections for mobiles
 let mobile = ['Pic0.jpg','Pic1.jpg','Pic6.jpg'];
+//create tha arrays to create the images callections for desktop
+let desktop =  ['Pic3.jpg','Pic5.jpg','Pic7.jpg'];
+//define the media qurey 
+let win = window.matchMedia("(max-width: 700px"));
+//check the media qurey is true & glase
+if(win.matches){
 //I change background images with setAttribute with media qurey
-document.getElementById("header").setAttribute("style","@media (max-width:600px){background-image: url(" + dir + mobile[randomChoice]");");
-}");
+document.getElementById("header").setAttribute("style","background-image: url(" + dir + mobile[randomChoice]");");
+}else{
+document.getElementById("header").setAttribute("style","background-image:url(" + dir + desktop[randomChoice]");");
+}
 
 
 
